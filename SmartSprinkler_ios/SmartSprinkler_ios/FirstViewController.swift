c224d0b4c2b0fe91a0d76b94e8ae763fe8d6c4c7
FishMage <schen358@wisc.edu>
2017-03-10 01:58:21 -0600

    Switch to iOS for better usage of AWS service

---
+++ SmartSprinkler_ios/SmartSprinkler_ios/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  SmartSprinkler_ios
+//
+//  Created by Richard Chen on 3/10/17.
+//  Copyright © 2017 Richard Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func locationOnClick(_ sender: UIButton) {
+        
+    }
+
+}
+
