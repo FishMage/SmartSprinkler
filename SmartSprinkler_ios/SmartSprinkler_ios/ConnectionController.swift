f94e5c1df52f2cd2bc456bad23144bb60f93980d
FishMage <schen358@wisc.edu>
2017-03-11 03:51:23 -0600

    iOS development

---
+++ SmartSprinkler_ios/SmartSprinkler_ios/ConnectionController.swift
@@ -0,0 +1,35 @@
+//
+//  ConnectionController.swift
+//  SmartSprinkler_ios
+//
+//  Created by Richard Chen on 3/10/17.
+//  Copyright © 2017 Richard Chen. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
